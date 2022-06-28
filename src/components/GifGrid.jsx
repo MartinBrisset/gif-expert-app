66f8cdcdb5e9a5e4b827fe77041f08d1e79cf380
martin <brissetmartin@gmail.com>
2022-06-28 13:11:29 -0300

    end

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,12 +1,24 @@
+import { GifItem } from "./GifItem"
+import { useFetchGifs } from "../hooks/useFetchGifs"
 
 export const GifGrid = ({category}) => {
 
-    
+    const {images, isLoading} = useFetchGifs(category)
 
     return (
         <>
             <h3>{category}</h3>
-            <p>Hola Mundo</p>
+            {
+                isLoading && (<h2>Loading...</h2>)
+            }
+            <div className="card-grid">
+                {images.map(img => (
+                    <GifItem 
+                        key={img.id} 
+                        {...img} 
+                    />
+                ))}
+            </div>
         </>
     )
 }
